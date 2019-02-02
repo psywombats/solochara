ec71df0adb74b7eedd75b3f863a3d06acdd535a7
psywombats <psy_wombats@wombatrpgs.net>
2019-02-02 03:21:34 -0800

    Slideybar script

---
+++ solochara/Assets/Scripts/UI/Battle/SliderBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderBar : MonoBehaviour {
+
+    public Mask backer;
+    public Image bar;
+    public int inset = 16;
+    public float ratio = 0.5f;
+
+    public void Update() {
+        UpdateScale();
+    }
+
+    public void OnValidate() {
+        UpdateScale();
+    }
+
+    private void UpdateScale() {
+        float maxOffset = bar.rectTransform.rect.width - inset;
+        bar.rectTransform.localPosition = new Vector3(
+            -1.0f * (1.0f - ratio) * maxOffset,
+            bar.rectTransform.localPosition.y,
+            bar.rectTransform.localPosition.z);
+    }
+}
